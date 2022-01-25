494da75736d709c087dcd34849fe5b61611ff16f
Dariusz Depta <dariusz.depta@dmntk.io>
2022-01-25 19:25:13 +0100

    Added CLI xdt test.

--- tests/cli/xdt/run.sh
+++ tests/cli/xdt/run.sh
@@ -3,8 +3,8 @@
 # remove existing output file
 rm -rf output.html
 
-# export FEEL expression to HTML
-dmntk xfe input.ctx input.feel output.html
+# export decision table to HTML
+dmntk xdt input.dtb output.html
 
 # send the generated HTML content to standard output
 cat output.html
